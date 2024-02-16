362c68122810dc91f413c6907d943d63c6a59306
Miguel Gomes Ferreira <miguel.gfe@outlook.com>
2024-02-15 21:35:57 -0300

    Add card for publication

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -53,6 +53,50 @@ export default function Home() {
           </div>
         </Accordion>
       </div>
+      <div className="w-3/4 p-4">
+        <h1 className="text-center">PUBLICAÇÕES</h1>
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <Image
+              className="rounded-t-lg"
+              src="/recisao-indireta.jpg"
+              alt=""
+              width={400}
+              height={400}
+            />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Rescisão indireta: o que é e quando pode ser aplicada
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            A rescisão indireta é caracterizada pela solicitação da demissão por parte do colaborador e se difere do pedido de demissão, pois, é realizada quando o empregador não cumpre a lei ou o acordo firmado no momento da contratação.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Continue Lendo
+              <svg
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
